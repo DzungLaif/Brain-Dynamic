a41d80db13bbec3737002b7186751dab3704b272
Laif Dzung <97878200+DzungLaif@users.noreply.github.com>
2024-06-05 20:49:37 +0300

    Update Leaky-Integrate-and-Fire.py

--- Leaky-Integrate-and-Fire.py
+++ Leaky-Integrate-and-Fire.py
@@ -1,1 +1,174 @@
-a
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import widgets
+from IPython.display import YouTubeVideo
+from IPython.display import IFrame
+from IPython.display import display
+import logging
+logging.getLogger('matplotlib.font_manager').disabled = True
+
+import ipywidgets as widgets
+%config InlineBackend.figure_format = 'retina'
+plt.style.use("https://raw.githubusercontent.com/NeuromatchAcademy/course-content/main/nma.mplstyle")
+my_layout = widgets.Layout()
+
+def plot_volt_trace(pars, v, sp):
+  V_th = pars['V_th']
+  dt, range_t = pars['dt'], pars['range_t']
+  if sp.size:
+    sp_num = (sp / dt).astype(int) - 1
+    v[sp_num] += 20  # draw nicer spikes
+
+  plt.plot(pars['range_t'], v, 'b')
+  plt.axhline(V_th, 0, 1, color='k', ls='--')
+  plt.xlabel('Time (ms)')
+  plt.ylabel('V (mV)')
+  plt.legend(['Membrane\npotential', r'Threshold V$_{\mathrm{th}}$'],
+             loc=[1.05, 0.75])
+  plt.ylim([-80, -40])
+  plt.show()
+
+def plot_GWN(pars, I_GWN):
+  plt.figure(figsize=(12, 4))
+  plt.subplot(121)
+  plt.plot(pars['range_t'][::3], I_GWN[::3], 'b')
+  plt.xlabel('Time (ms)')
+  plt.ylabel(r'$I_{GWN}$ (pA)')
+  plt.subplot(122)
+  plot_volt_trace(pars, v, sp)
+  plt.tight_layout()
+  plt.show()
+
+def my_hists(isi1, isi2, cv1, cv2, sigma1, sigma2):
+  plt.figure(figsize=(11, 4))
+  my_bins = np.linspace(10, 30, 20)
+  plt.subplot(121)
+  plt.hist(isi1, bins=my_bins, color='b', alpha=0.5)
+  plt.xlabel('ISI (ms)')
+  plt.ylabel('count')
+  plt.title(r'$\sigma_{GWN}=$%.1f, CV$_{\mathrm{isi}}$=%.3f' % (sigma1, cv1))
+  plt.subplot(122)
+  plt.hist(isi2, bins=my_bins, color='b', alpha=0.5)
+  plt.xlabel('ISI (ms)')
+  plt.ylabel('count')
+  plt.title(r'$\sigma_{GWN}=$%.1f, CV$_{\mathrm{isi}}$=%.3f' % (sigma2, cv2))
+  plt.tight_layout()
+  plt.show()
+
+class PlayVideo(IFrame):
+  def __init__(self, id, source, page=1, width=400, height=300, **kwargs):
+    self.id = id
+    if source == 'Bilibili':
+      src = f'https://player.bilibili.com/player.html?bvid={id}&page={page}'
+    elif source == 'Osf':
+      src = f'https://mfr.ca-1.osf.io/render?url=https://osf.io/download/{id}/?direct%26mode=render'
+    super(PlayVideo, self).__init__(src, width, height, **kwargs)
+
+
+def display_videos(video_ids, W=400, H=300, fs=1):
+  tab_contents = []
+  for i, video_id in enumerate(video_ids):
+    out = widgets.Output()
+    with out:
+      if video_ids[i][0] == 'Youtube':
+        video = YouTubeVideo(id=video_ids[i][1], width=W, height=H, fs=fs, rel=0)
+        print(f'Video available at https://youtube.com/watch?v={video.id}')
+      else:
+        video = PlayVideo(id=video_ids[i][1], source=video_ids[i][0], width=W, height=H, fs=fs, autoplay=False)
+        if video_ids[i][0] == 'Bilibili':
+          print(f'Video available at https://www.bilibili.com/video/{video.id}')
+        elif video_ids[i][0] == 'Osf':
+          print(f'Video available at https://osf.io/{video.id}')
+      display(video)
+    tab_contents.append(out)
+  return tab_contents
+
+def default_pars(**kwargs):
+  pars = {}
+  pars['V_th'] = -55.     # spike threshold [mV]
+  pars['V_reset'] = -75.  # reset potential [mV]
+  pars['tau_m'] = 10.     # membrane time constant [ms]
+  pars['g_L'] = 10.       # leak conductance [nS]
+  pars['V_init'] = -75.   # initial potential [mV]
+  pars['E_L'] = -75.      # leak reversal potential [mV]
+  pars['tref'] = 2.       # refractory time (ms)
+  pars['T'] = 400.  # Total duration of simulation [ms]
+  pars['dt'] = .1   # Simulation time step [ms]
+  for k in kwargs:
+    pars[k] = kwargs[k]
+  pars['range_t'] = np.arange(0, pars['T'], pars['dt'])  # Vector of discretized time points [ms]
+  return pars
+
+def run_LIF(pars, Iinj, stop=False):
+  # Set parameters
+  V_th, V_reset = pars['V_th'], pars['V_reset']
+  tau_m, g_L = pars['tau_m'], pars['g_L']
+  V_init, E_L = pars['V_init'], pars['E_L']
+  dt, range_t = pars['dt'], pars['range_t']
+  Lt = range_t.size
+  tref = pars['tref']
+
+  v = np.zeros(Lt)
+  v[0] = V_init
+
+  Iinj = Iinj * np.ones(Lt)
+  
+  if stop:
+    Iinj[:int(len(Iinj) / 2) - 1000] = 0
+    Iinj[int(len(Iinj) / 2) + 1000:] = 0
+
+  rec_spikes = []  # record spike times
+  tr = 0.  # the count for refractory duration
+
+  for it in range(Lt - 1):
+    if tr > 0:  # check if in refractory period
+      v[it] = V_reset  # set voltage to reset
+      tr = tr - 1 # reduce running counter of refractory period
+    elif v[it] >= V_th:  # if voltage over threshold
+      rec_spikes.append(it)  # record spike event
+      v[it] = V_reset  # reset voltage
+      tr = tref / dt  # set refractory time
+    dv = (-(v[it] - E_L) + Iinj[it] / g_L) * (dt / tau_m)
+    v[it + 1] = v[it] + dv
+  rec_spikes = np.array(rec_spikes) * dt
+  return v, rec_spikes
+
+def my_GWN(pars, mu, sig, myseed=False):
+  # Retrieve simulation parameters
+  dt, range_t = pars['dt'], pars['range_t']
+  Lt = range_t.size
+  # Set random seed
+  if myseed:
+      np.random.seed(seed=myseed)
+  else:
+      np.random.seed()
+  # Generate GWN
+  I_gwn = mu + sig * np.random.randn(Lt) / np.sqrt(dt / 1000.)
+  return I_gwn
+
+# Neuron response with constant DC input
+"""
+def diff_DC(I_dc=200., tau_m=10.):
+  pars = default_pars(T=100.)
+  pars['tau_m'] = tau_m
+  v, sp = run_LIF(pars, Iinj=I_dc)
+  plot_volt_trace(pars, v, sp)
+  plt.show()
+"""
+
+# Neuron response with Gaussian white noise (GWN) current
+"""
+def diff_GWN_to_LIF(mu_gwn, sig_gwn):
+  pars = default_pars(T=100.)
+  I_GWN = my_GWN(pars, mu=mu_gwn, sig=sig_gwn)
+  v, sp = run_LIF(pars, Iinj=I_GWN)
+  plt.figure(figsize=(12, 4))
+  plt.subplot(121)
+  plt.plot(pars['range_t'][::3], I_GWN[::3], 'b')
+  plt.xlabel('Time (ms)')
+  plt.ylabel(r'$I_{GWN}$ (pA)')
+  plt.subplot(122)
+  plot_volt_trace(pars, v, sp)
+  plt.tight_layout()
+  plt.show()
+"""
