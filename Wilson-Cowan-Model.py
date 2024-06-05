50bdd641f11388f44cedbe0ccf3ce80ecda21dec
Laif Dzung <97878200+DzungLaif@users.noreply.github.com>
2024-06-05 23:00:48 +0300

    Update Wilson-Cowan-Model.py

--- Wilson-Cowan-Model.py
+++ Wilson-Cowan-Model.py
@@ -1,1 +1,259 @@
-a
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as opt
+import logging
+from ipywidgets import widgets
+from IPython.display import YouTubeVideo
+from IPython.display import IFrame
+from IPython.display import display
+logging.getLogger('matplotlib.font_manager').disabled = True
+import ipywidgets as widgets  # interactive display
+%config InlineBackend.figure_format = 'retina'
+plt.style.use("https://raw.githubusercontent.com/NeuromatchAcademy/course-content/main/nma.mplstyle")
+
+# @title Plotting Functions
+
+def plot_FI_inverse(x, a, theta):
+  f, ax = plt.subplots()
+  ax.plot(x, F_inv(x, a=a, theta=theta))
+  ax.set(xlabel="$x$", ylabel="$F^{-1}(x)$")
+
+
+def plot_FI_EI(x, FI_exc, FI_inh):
+  plt.figure()
+  plt.plot(x, FI_exc, 'b', label='E population')
+  plt.plot(x, FI_inh, 'r', label='I population')
+  plt.legend(loc='lower right')
+  plt.xlabel('x (a.u.)')
+  plt.ylabel('F(x)')
+  plt.show()
+
+
+def my_test_plot(t, rE1, rI1, rE2, rI2):
+  plt.figure()
+  ax1 = plt.subplot(211)
+  ax1.plot(pars['range_t'], rE1, 'b', label='E population')
+  ax1.plot(pars['range_t'], rI1, 'r', label='I population')
+  ax1.set_ylabel('Activity')
+  ax1.legend(loc='best')
+  ax2 = plt.subplot(212, sharex=ax1, sharey=ax1)
+  ax2.plot(pars['range_t'], rE2, 'b', label='E population')
+  ax2.plot(pars['range_t'], rI2, 'r', label='I population')
+  ax2.set_xlabel('t (ms)')
+  ax2.set_ylabel('Activity')
+  ax2.legend(loc='best')
+  
+
+def plot_nullclines(Exc_null_rE, Exc_null_rI, Inh_null_rE, Inh_null_rI):
+  plt.figure()
+  plt.plot(Exc_null_rE, Exc_null_rI, 'b', label='E nullcline')
+  plt.plot(Inh_null_rE, Inh_null_rI, 'r', label='I nullcline')
+  plt.xlabel(r'$r_E$')
+  plt.ylabel(r'$r_I$')
+  plt.legend(loc='best')
+  plt.show()
+
+
+def my_plot_nullcline(pars):
+  Exc_null_rE = np.linspace(-0.01, 0.96, 100)
+  Exc_null_rI = get_E_nullcline(Exc_null_rE, **pars)
+  Inh_null_rI = np.linspace(-.01, 0.8, 100)
+  Inh_null_rE = get_I_nullcline(Inh_null_rI, **pars)
+  plt.plot(Exc_null_rE, Exc_null_rI, 'b', label='E nullcline')
+  plt.plot(Inh_null_rE, Inh_null_rI, 'r', label='I nullcline')
+  plt.xlabel(r'$r_E$')
+  plt.ylabel(r'$r_I$')
+  plt.legend(loc='best')
+
+
+def my_plot_vector(pars, my_n_skip=2, myscale=5):
+  EI_grid = np.linspace(0., 1., 20)
+  rE, rI = np.meshgrid(EI_grid, EI_grid)
+  drEdt, drIdt = EIderivs(rE, rI, **pars)
+  n_skip = my_n_skip
+  plt.quiver(rE[::n_skip, ::n_skip], rI[::n_skip, ::n_skip],
+             drEdt[::n_skip, ::n_skip], drIdt[::n_skip, ::n_skip],
+             angles='xy', scale_units='xy', scale=myscale, facecolor='c')
+  plt.xlabel(r'$r_E$')
+  plt.ylabel(r'$r_I$')
+
+
+def my_plot_trajectory(pars, mycolor, x_init, mylabel):
+  pars = pars.copy()
+  pars['rE_init'], pars['rI_init'] = x_init[0], x_init[1]
+  rE_tj, rI_tj = simulate_wc(**pars)
+  plt.plot(rE_tj, rI_tj, color=mycolor, label=mylabel)
+  plt.plot(x_init[0], x_init[1], 'o', color=mycolor, ms=8)
+  plt.xlabel(r'$r_E$')
+  plt.ylabel(r'$r_I$')
+
+
+def my_plot_trajectories(pars, dx, n, mylabel):
+  pars = pars.copy()
+  for ie in range(n):
+    for ii in range(n):
+      pars['rE_init'], pars['rI_init'] = dx * ie, dx * ii
+      rE_tj, rI_tj = simulate_wc(**pars)
+      if (ie == n-1) & (ii == n-1):
+          plt.plot(rE_tj, rI_tj, 'gray', alpha=0.8, label=mylabel)
+      else:
+          plt.plot(rE_tj, rI_tj, 'gray', alpha=0.8)
+  plt.xlabel(r'$r_E$')
+  plt.ylabel(r'$r_I$')
+
+
+def plot_complete_analysis(pars):
+  plt.figure(figsize=(7.7, 6.))
+  my_plot_trajectories(pars, 0.2, 6,'Sample trajectories \nfor different init. conditions')
+  my_plot_trajectory(pars, 'orange', [0.6, 0.8],'Sample trajectory for \nlow activity')
+  my_plot_trajectory(pars, 'm', [0.6, 0.6],'Sample trajectory for \nhigh activity')
+  my_plot_nullcline(pars)
+  # plot vector field
+  EI_grid = np.linspace(0., 1., 20)
+  rE, rI = np.meshgrid(EI_grid, EI_grid)
+  drEdt, drIdt = EIderivs(rE, rI, **pars)
+  n_skip = 2
+  plt.quiver(rE[::n_skip, ::n_skip], rI[::n_skip, ::n_skip],
+             drEdt[::n_skip, ::n_skip], drIdt[::n_skip, ::n_skip],
+             angles='xy', scale_units='xy', scale=5., facecolor='c')
+  plt.legend(loc=[1.02, 0.57], handlelength=1)
+
+
+def plot_fp(x_fp, position=(0.02, 0.1), rotation=0):
+  plt.plot(x_fp[0], x_fp[1], 'ko', ms=8)
+  plt.text(x_fp[0] + position[0], x_fp[1] + position[1],
+           f'Fixed Point1=\n({x_fp[0]:.3f}, {x_fp[1]:.3f})',
+           horizontalalignment='center', verticalalignment='bottom',
+           rotation=rotation)
+  
+
+def default_pars(**kwargs):
+  pars = {}
+  pars['tau_E'] = 1.     # Timescale of the E population [ms]
+  pars['a_E'] = 1.2      # Gain of the E population
+  pars['theta_E'] = 2.8  # Threshold of the E population
+  pars['tau_I'] = 2.0    # Timescale of the I population [ms]
+  pars['a_I'] = 1.0      # Gain of the I population
+  pars['theta_I'] = 4.0  # Threshold of the I population
+  pars['wEE'] = 9.   # E to E
+  pars['wEI'] = 4.   # I to E
+  pars['wIE'] = 13.  # E to I
+  pars['wII'] = 11.  # I to I
+  pars['I_ext_E'] = 0.
+  pars['I_ext_I'] = 0.
+  pars['T'] = 50.        # Total duration of simulation [ms]
+  pars['dt'] = .1        # Simulation time step [ms]
+  pars['rE_init'] = 0.2  # Initial value of E
+  pars['rI_init'] = 0.2  # Initial value of I
+  for k in kwargs:
+      pars[k] = kwargs[k]
+  # Vector of discretized time points [ms]
+  pars['range_t'] = np.arange(0, pars['T'], pars['dt'])
+  return pars
+
+
+def F(x, a, theta):
+  f = (1 + np.exp(-a * (x - theta)))**-1 - (1 + np.exp(a * theta))**-1
+  return f
+
+
+def dF(x, a, theta):
+  dFdx = a * np.exp(-a * (x - theta)) * (1 + np.exp(-a * (x - theta)))**-2
+  return dFdx
+
+
+class PlayVideo(IFrame):
+  def __init__(self, id, source, page=1, width=400, height=300, **kwargs):
+    self.id = id
+    if source == 'Bilibili':
+      src = f'https://player.bilibili.com/player.html?bvid={id}&page={page}'
+    elif source == 'Osf':
+      src = f'https://mfr.ca-1.osf.io/render?url=https://osf.io/download/{id}/?direct%26mode=render'
+    super(PlayVideo, self).__init__(src, width, height, **kwargs)
+
+
+def display_videos(video_ids, W=400, H=300, fs=1):
+  tab_contents = []
+  for i, video_id in enumerate(video_ids):
+    out = widgets.Output()
+    with out:
+      if video_ids[i][0] == 'Youtube':
+        video = YouTubeVideo(id=video_ids[i][1], width=W,
+                             height=H, fs=fs, rel=0)
+        print(f'Video available at https://youtube.com/watch?v={video.id}')
+      else:
+        video = PlayVideo(id=video_ids[i][1], source=video_ids[i][0], width=W,
+                          height=H, fs=fs, autoplay=False)
+        if video_ids[i][0] == 'Bilibili':
+          print(f'Video available at https://www.bilibili.com/video/{video.id}')
+        elif video_ids[i][0] == 'Osf':
+          print(f'Video available at https://osf.io/{video.id}')
+      display(video)
+    tab_contents.append(out)
+  return tab_contents
+
+
+def simulate_wc(tau_E, a_E, theta_E, tau_I, a_I, theta_I, wEE, wEI, wIE, wII, I_ext_E, I_ext_I, rE_init, rI_init, dt, range_t, **other_pars):
+  Lt = range_t.size
+  rE = np.append(rE_init, np.zeros(Lt - 1))
+  rI = np.append(rI_init, np.zeros(Lt - 1))
+  I_ext_E = I_ext_E * np.ones(Lt)
+  I_ext_I = I_ext_I * np.ones(Lt)
+  for k in range(Lt - 1):
+    drE = dt / tau_E * (-rE[k] + F(wEE * rE[k] - wEI * rI[k] + I_ext_E[k], a_E, theta_E))
+    drI = dt / tau_I * (-rI[k] + F(wIE * rE[k] - wII * rI[k] + I_ext_I[k], a_I, theta_I))
+    rE[k + 1] = rE[k] + drE
+    rI[k + 1] = rI[k] + drI
+  return rE, rI
+
+
+def plot_EI_diffinitial(rE_init=0.0):
+  pars = default_pars(rE_init=rE_init, rI_init=.15)
+  rE, rI = simulate_wc(**pars)
+  plt.figure()
+  plt.plot(pars['range_t'], rE, 'b', label='E population')
+  plt.plot(pars['range_t'], rI, 'r', label='I population')
+  plt.xlabel('t (ms)')
+  plt.ylabel('Activity')
+  plt.legend(loc='best')
+  plt.show()
+
+
+def plot_activity_phase(n_t):
+  plt.figure(figsize=(8, 5.5))
+  plt.subplot(211)
+  plt.plot(pars['range_t'], rE, 'b', label=r'$r_E$')
+  plt.plot(pars['range_t'], rI, 'r', label=r'$r_I$')
+  plt.plot(pars['range_t'][n_t], rE[n_t], 'bo')
+  plt.plot(pars['range_t'][n_t], rI[n_t], 'ro')
+  plt.axvline(pars['range_t'][n_t], 0, 1, color='k', ls='--')
+  plt.xlabel('t (ms)', fontsize=14)
+  plt.ylabel('Activity', fontsize=14)
+  plt.legend(loc='best', fontsize=14)
+  plt.subplot(212)
+  plt.plot(rE, rI, 'k')
+  plt.plot(rE[n_t], rI[n_t], 'ko')
+  plt.xlabel(r'$r_E$', fontsize=18, color='b')
+  plt.ylabel(r'$r_I$', fontsize=18, color='r')
+  plt.tight_layout()
+  plt.show()
+
+
+def FIcurve():
+  pars = default_pars()
+  x = np.arange(0, 10, .1)
+  print(pars['a_E'], pars['theta_E'])
+  print(pars['a_I'], pars['theta_I'])
+  # Compute the F-I curve of the excitatory population
+  FI_exc = F(x, pars['a_E'], pars['theta_E'])
+  # Compute the F-I curve of the inhibitory population
+  FI_inh = F(x, pars['a_I'], pars['theta_I'])
+  # Visualize
+  with plt.xkcd():
+    plot_FI_EI(x, FI_exc, FI_inh)
+
+
+def EIderivs(rE, rI, tau_E, a_E, theta_E, wEE, wEI, I_ext_E, tau_I, a_I, theta_I, wIE, wII, I_ext_I, **other_pars):
+  drEdt = (-rE + F(wEE * rE - wEI * rI + I_ext_E, a_E, theta_E)) / tau_E
+  drIdt = (-rI + F(wIE * rE - wII * rI + I_ext_I, a_I, theta_I)) / tau_I
+  return drEdt, drIdt
