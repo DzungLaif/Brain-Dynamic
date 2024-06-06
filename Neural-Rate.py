af4b92c47e6bc8c8008ae38d5f1dfdacd7b43e35
Laif Dzung <97878200+DzungLaif@users.noreply.github.com>
2024-06-06 10:47:33 +0300

    Update Neural-Rate.py

--- Neural-Rate.py
+++ Neural-Rate.py
@@ -1,1 +1,180 @@
-a
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as opt  # root-finding algorithm
+import logging
+from ipywidgets import widgets
+from IPython.display import YouTubeVideo
+from IPython.display import IFrame
+from IPython.display import display
+logging.getLogger('matplotlib.font_manager').disabled = True
+import ipywidgets as widgets  # interactive display
+%config InlineBackend.figure_format = 'retina'
+plt.style.use("https://raw.githubusercontent.com/NeuromatchAcademy/course-content/main/nma.mplstyle")
+
+
+def plot_fI(x, f):
+  plt.figure(figsize=(6, 4))  # plot the figure
+  plt.plot(x, f, 'k')
+  plt.xlabel('x (a.u.)', fontsize=14)
+  plt.ylabel('F(x)', fontsize=14)
+  plt.show()
+
+
+def plot_dr_r(r, drdt, x_fps=None):
+  plt.figure()
+  plt.plot(r, drdt, 'k')
+  plt.plot(r, 0. * r, 'k--')
+  if x_fps is not None:
+    plt.plot(x_fps, np.zeros_like(x_fps), "ko", ms=12)
+  plt.xlabel(r'$r$')
+  plt.ylabel(r'$\frac{dr}{dt}$', fontsize=20)
+  plt.ylim(-0.1, 0.1)
+  plt.show()
+
+
+def plot_dFdt(x, dFdt):
+  plt.figure()
+  plt.plot(x, dFdt, 'r')
+  plt.xlabel('x (a.u.)', fontsize=14)
+  plt.ylabel('dF(x)', fontsize=14)
+  plt.show()
+
+
+class PlayVideo(IFrame):
+  def __init__(self, id, source, page=1, width=400, height=300, **kwargs):
+    self.id = id
+    if source == 'Bilibili':
+      src = f'https://player.bilibili.com/player.html?bvid={id}&page={page}'
+    elif source == 'Osf':
+      src = f'https://mfr.ca-1.osf.io/render?url=https://osf.io/download/{id}/?direct%26mode=render'
+    super(PlayVideo, self).__init__(src, width, height, **kwargs)
+
+
+def display_videos(video_ids, W=400, H=300, fs=1):
+  tab_contents = []
+  for i, video_id in enumerate(video_ids):
+    out = widgets.Output()
+    with out:
+      if video_ids[i][0] == 'Youtube':
+        video = YouTubeVideo(id=video_ids[i][1], width=W,
+                             height=H, fs=fs, rel=0)
+        print(f'Video available at https://youtube.com/watch?v={video.id}')
+      else:
+        video = PlayVideo(id=video_ids[i][1], source=video_ids[i][0], width=W,
+                          height=H, fs=fs, autoplay=False)
+        if video_ids[i][0] == 'Bilibili':
+          print(f'Video available at https://www.bilibili.com/video/{video.id}')
+        elif video_ids[i][0] == 'Osf':
+          print(f'Video available at https://osf.io/{video.id}')
+      display(video)
+    tab_contents.append(out)
+  return tab_contents
+
+
+def default_pars_single(**kwargs):
+  pars = {}
+  pars['tau'] = 1.     # Timescale of the E population [ms]
+  pars['a'] = 1.2      # Gain of the E population
+  pars['theta'] = 2.8  # Threshold of the E population
+  pars['w'] = 0.  # E to E, we first set it to 0
+  pars['I_ext'] = 0.
+  pars['T'] = 20.       # Total duration of simulation [ms]
+  pars['dt'] = .1       # Simulation time step [ms]
+  pars['r_init'] = 0.2  # Initial value of E
+  pars.update(kwargs)
+  pars['range_t'] = np.arange(0, pars['T'], pars['dt'])
+  return pars
+
+
+def F(x, a, theta):
+  # Define the sigmoidal transfer function f = F(x)
+  f = (1 + np.exp(-a * (x - theta)))**-1 - (1 + np.exp(a * theta))**-1
+  return f
+
+
+def interactive_plot_FI(a, theta):
+  x = np.arange(0, 10, .1)
+  plt.figure()
+  plt.plot(x, F(x, a, theta), 'k')
+  plt.xlabel('x (a.u.)', fontsize=14)
+  plt.ylabel('F(x)', fontsize=14)
+  plt.show()
+
+
+def simulate_single(pars):
+  # Set parameters
+  tau, a, theta = pars['tau'], pars['a'], pars['theta']
+  w = pars['w']
+  I_ext = pars['I_ext']
+  r_init = pars['r_init']
+  dt, range_t = pars['dt'], pars['range_t']
+  Lt = range_t.size
+  # Initialize activity
+  r = np.zeros(Lt)
+  r[0] = r_init
+  I_ext = I_ext * np.ones(Lt)
+  # Update the E activity
+  for k in range(Lt - 1):
+      dr = dt / tau * (-r[k] + F(w * r[k] + I_ext[k], a, theta))
+      r[k+1] = r[k] + dr
+  return r
+
+
+def Myplot_E_diffI_difftau(I_ext, tau):
+  # set external input and time constant
+  pars['I_ext'] = I_ext
+  pars['tau'] = tau
+  # simulation
+  r = simulate_single(pars)
+  # Analytical Solution
+  r_ana = (pars['r_init']
+           + (F(I_ext, pars['a'], pars['theta'])
+           - pars['r_init']) * (1. - np.exp(-pars['range_t'] / pars['tau'])))
+  # plot
+  plt.figure()
+  plt.plot(pars['range_t'], r, 'b', label=r'$r_{\mathrm{sim}}$(t)', alpha=0.5,
+           zorder=1)
+  plt.plot(pars['range_t'], r_ana, 'b--', lw=5, dashes=(2, 2),
+           label=r'$r_{\mathrm{ana}}$(t)', zorder=2)
+  plt.plot(pars['range_t'],
+           F(I_ext, pars['a'], pars['theta']) * np.ones(pars['range_t'].size),
+           'k--', label=r'$F(I_{\mathrm{ext}})$')
+  plt.xlabel('t (ms)', fontsize=16.)
+  plt.ylabel('Activity r(t)', fontsize=16.)
+  plt.legend(loc='best', fontsize=14.)
+  plt.show()
+
+
+def compute_drdt(r, I_ext, w, a, theta, tau, **other_pars):
+  # Calculate drdt
+  drdt = (-r + F(w * r + I_ext, a, theta)) / tau
+  return drdt
+
+
+def my_fp_single(r_guess, a, theta, w, I_ext, **other_pars):
+  # define the right hand of E dynamics
+  def my_WCr(x):
+    r = x
+    drdt = (-r + F(w * r + I_ext, a, theta))
+    y = np.array(drdt)
+    return y
+  x0 = np.array(r_guess)
+  x_fp = opt.root(my_WCr, x0).x.item()
+  return x_fp
+
+
+def check_fp_single(x_fp, a, theta, w, I_ext, mytol=1e-4, **other_pars):
+  # calculate Equation(3)
+  y = x_fp - F(w * x_fp + I_ext, a, theta)
+  # Here we set tolerance as 10^{-4}
+  return np.abs(y) < mytol
+
+
+def my_fp_finder(pars, r_guess_vector, mytol=1e-4):
+  x_fps = []
+  correct_fps = []
+  for r_guess in r_guess_vector:
+    x_fp = my_fp_single(r_guess, **pars)
+    if check_fp_single(x_fp, **pars, mytol=mytol):
+      x_fps.append(x_fp)
+  return x_fps
